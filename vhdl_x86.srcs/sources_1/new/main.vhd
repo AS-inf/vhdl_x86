b20f07d8562cc4768cdb7b4b305fe38cc2a41125
AS-inf <a.inf.wroc@gmail.com>
2021-02-28 13:33:04 +0100

    output test; synthesis test; sim test;

--- vhdl_x86.srcs/sources_1/new/main.vhd
+++ vhdl_x86.srcs/sources_1/new/main.vhd
@@ -21,23 +21,20 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity main is
---  Port ( );
+port
+(
+       out_test: out std_logic
+);
 end main;
 
 architecture Behavioral of main is
 
 begin
-
+out_test <= '1';
 
 end Behavioral;
